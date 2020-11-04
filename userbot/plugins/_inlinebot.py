feb12673641b5b39ff203751611515d108fe6330
Prastiko Sasmitho <70458149+Akarata@users.noreply.github.com>
2020-11-04 09:58:15 +0700

    Update _inlinebot.py

--- userbot/plugins/_inlinebot.py
+++ userbot/plugins/_inlinebot.py
@@ -159,7 +159,7 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
             # https://t.me/TelethonChat/115200
             await event.edit(buttons=buttons)
         else:
-            reply_pop_up_alert = "Yeh mera maal hai !! Haat kese laga diya be tune ise? \n\nNikal, jaake apna catuserbot bana..."
+            reply_pop_up_alert = "Iri bilang boss!!! \n\nKalo mau Deploy sendiri lah Anjim"
             await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
 
     @tgbot.on(
@@ -176,7 +176,7 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
             # https://t.me/TelethonChat/115200
             await event.edit(buttons=buttons)
         else:
-            reply_pop_up_alert = "Yeh mera maal hai !! Haat kese laga diya be tune ise? \n\nNikal, jaake apna catuserbot bana..."
+            reply_pop_up_alert = "Iri bilang boss!!! \n\nKalo mau Deploy sendiri lah Anjim"
             await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
 
     @tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"secret_(.*)")))
@@ -219,7 +219,7 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
             else:
                 reply_pop_up_alert = help_string
             reply_pop_up_alert += (
-                "Use .unload {} to remove this plugin ©catuserbot".format(plugin_name)
+                "Use .unload {} to remove this plugin ©catuserbot ( Modified @Akarata )".format(plugin_name)
             )
             try:
                 await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
@@ -236,7 +236,7 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
                     )
         else:
             reply_pop_up_alert = (
-                "Iri bilang boss!!! \n\nKalo mau Deploy sendiri lah anjim..."
+                "Iri bilang boss!!! \n\nKalo mau Deploy sendiri lah Anjim..."
             )
             await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
 
@@ -245,7 +245,7 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
         if event.query.user_id == bot.uid:
             await event.edit("menu closed")
         else:
-            reply_pop_up_alert = "Yeh mera maal hai !! Haat kese laga diya be tune ise? \n\nNikal, jaake apna catuserbot bana..."
+            reply_pop_up_alert = "Iri bilang boss!!! \n\nKalo mau Deploy sendiri lah Anjim"
             await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
 
     @tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"stats")))
