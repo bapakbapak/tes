ba671ef16bbfc1b837ea036740dc25b2b857be2e
Prastiko Sasmitho <70458149+Akarata@users.noreply.github.com>
2020-11-04 07:15:19 +0700

    misc.py fix

--- userbot/plugins/misc.py
+++ userbot/plugins/misc.py
@@ -101,7 +101,7 @@ async def repeat(rep):
 async def repo_is_here(wannasee):
     """ For .repo command, just returns the repo URL. """
     await wannasee.edit(
-        "[Klik disini](https://github.com/Akarata/AkaProject) untuk membuka Akarata_Project :3"
+        "[Klik disini](https://github.com/Akarata/AkaProject) untuk membuka Akarata_Project "
     )
 
 
